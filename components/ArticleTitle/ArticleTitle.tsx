6a3c208112c723887849f0c8fcf1cf2cb8793e7f
RajThapa7 <jyangoraj123@gmail.com>
2023-04-29 19:36:15 +0545

    add:components added

---
+++ components/ArticleTitle/ArticleTitle.tsx
@@ -0,0 +1,7 @@
+export default function ArticleTitle() {
+  return (
+    <p className="transition-smooth w-fit cursor-pointer text-2xl font-semibold underline-offset-4 hover:text-blue-600 hover:underline">
+      ArticleTitle &gt;
+    </p>
+  );
+}
