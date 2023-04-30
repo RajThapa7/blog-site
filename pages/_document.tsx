33635264fbe6c68b23be2a7098a45ddb4380ef24
RajThapa7 <jyangoraj123@gmail.com>
2023-04-30 14:55:11 +0545

    add:tailwindcss debug screens

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&family=Outfit:wght@300;400;500&display=swap"
+        rel="stylesheet"
+      ></link>
+      <body className="debug-screens">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
