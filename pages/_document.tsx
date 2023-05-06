3d27f505fd7eca6dd4555d712faccde98cb6d823
RajThapa7 <jyangoraj123@gmail.com>
2023-05-06 07:05:13 +0545

    modified:google font added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,10 @@ export default function Document() {
       <Head />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat&family=Outfit:wght@300;400;500&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&family=Outfit:wght@300;400;500&display=swap"
         rel="stylesheet"
       ></link>
       <body className="debug-screens">
