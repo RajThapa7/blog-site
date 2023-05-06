60ab11d8320a9b8da7ecd56d1ec60ade745e879d
RajThapa7 <jyangoraj123@gmail.com>
2023-05-06 06:53:47 +0545

    article title modified

--- components/ArticleTitle/ArticleTitle.tsx
+++ components/ArticleTitle/ArticleTitle.tsx
@@ -1,7 +1,11 @@
-export default function ArticleTitle() {
+interface IArticleTiltelProps {
+  children: React.ReactNode;
+}
+
+export default function ArticleTitle({ children }: IArticleTiltelProps) {
   return (
-    <p className="transition-smooth w-fit cursor-pointer text-2xl font-semibold underline-offset-4 hover:text-blue-600 hover:underline">
-      ArticleTitle &gt;
+    <p className="transition-smooth flex w-fit cursor-pointer items-center gap-x-1 text-xl font-semibold decoration-blue-600 underline-offset-4">
+      {children}
     </p>
   );
 }
