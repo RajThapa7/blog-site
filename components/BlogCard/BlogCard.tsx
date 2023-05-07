20d980aad4020eb4dd68128966bfb5205c9ed268
RajThapa7 <jyangoraj123@gmail.com>
2023-05-07 11:42:51 +0545

    modified:BlogCard component modified

--- components/BlogCard/BlogCard.tsx
+++ components/BlogCard/BlogCard.tsx
@@ -1,68 +1,93 @@
-import Image, { StaticImageData } from "next/image";
-import { BsDot } from "react-icons/bs";
-import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import useFetchFeaturedMedia from "@/features/articles/api/hooks/useFetchFeaturedImage";
+import fallbackImg from "public/wallpaper.jpg";
+import Link from "next/link";
+import { BsDot } from "react-icons/bs";
+import useFetchAuthor from "@/features/articles/api/hooks/useFetchAuthor";
+import sanitizeHtml from "sanitize-html";
 
 export interface IBlogCardProps {
-  title: string;
-  author: string;
   date: string;
-  blogTitleImg: StaticImageData | string;
-  authorImg: StaticImageData | string;
-  readTime: number;
+  title: Title;
+  excerpt: Excerpt;
+  author: number;
+  featured_media: number;
+}
+
+export interface Excerpt {
+  rendered: string;
+  protected: boolean;
+}
+
+export interface Title {
+  rendered: string;
 }
 
 export default function BlogCard({
   title,
   author,
   date,
-  blogTitleImg,
-  authorImg,
-  readTime,
+  excerpt,
+  featured_media,
 }: IBlogCardProps) {
   const router = useRouter();
+
+  const blogTitleImg = useFetchFeaturedMedia(featured_media)?.source_url;
+
+  const authorDetails = useFetchAuthor(author);
+  const authorName = authorDetails?.name;
+  const authorImg = authorDetails?.avatar_urls?.["96"];
+
   return (
     <div
-      className="transition-smooth z-0 h-fit w-fit rounded-md bg-white px-4 py-6 pt-4 shadow-lg hover:bg-gray-100"
+      className="transition-smooth z-0 h-fit w-fit cursor-pointer rounded-md bg-white px-4 py-6 pt-4 shadow-lg hover:bg-gray-100"
       onClick={(e) => {
         router.push("/blog");
       }}
     >
-      <div className="h-fit w-80">
+      <div className="h-auto w-[350px]">
         <Image
-          src={blogTitleImg}
+          src={blogTitleImg || fallbackImg}
           alt="blog-image"
-          className="rounded-md object-contain"
+          className="aspect-[16/10] rounded-md object-fill"
+          height={300}
+          width={400}
         />
-        <p className="transition-smooth cursor-pointer py-4 text-lg font-semibold text-gray-700 underline-offset-2 hover:underline">
-          {title}
+        <p className="transition-smooth w-fit cursor-pointer py-4 text-lg font-semibold text-gray-700 underline-offset-2 hover:underline">
+          {title.rendered}
         </p>
+
+        <div
+          dangerouslySetInnerHTML={{ __html: sanitizeHtml(excerpt.rendered) }}
+          className="break-words"
+        />
 
         {/* author */}
         <div className="flex w-fit gap-x-3 pt-4">
-          <div className="h-16 w-16">
-            <Image
-              src={authorImg}
-              alt="profile-pic"
-              className="rounded-full object-contain"
-              onClick={(e) => {
-                e.stopPropagation();
-                router.push("#");
-              }}
-            />
-          </div>
+          <Image
+            src={authorImg || fallbackImg}
+            alt="profile-pic"
+            className=" rounded-full"
+            onClick={(e) => {
+              e.stopPropagation();
+              router.push("#");
+            }}
+            height={48}
+            width={48}
+          />
           <div className="w-fit self-center">
             <Link
               href="#"
               onClick={(e) => e.stopPropagation()}
               className="font-semibold text-gray-800"
             >
-              {author}
+              {authorName}
             </Link>
             <div className="flex gap-x-4">
-              <p>{date}</p>
+              <p>{date.slice(0, 10)}</p>
               <p className="flex items-center">
-                <BsDot /> {readTime}&nbsp;min read
+                <BsDot /> 10&nbsp;min read
               </p>
             </div>
           </div>
