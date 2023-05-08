33c8cfb8e91c032430d85d0ca7be8deca5e20e92
RajThapa7 <jyangoraj123@gmail.com>
2023-05-08 11:12:40 +0545

    add:nprogress bar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,14 @@ import MainLayout from "@/Layouts/MainLayout";
 import QueryProvider from "@/Layouts/QueryProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Router } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+NProgress.configure({ showSpinner: false });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
